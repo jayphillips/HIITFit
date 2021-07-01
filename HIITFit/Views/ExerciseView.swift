1d32a02e792263ccff60bdc842662f9a5cf9f706
Jay Phillips <jayphillips@icloud.com>
2021-07-01 13:55:50 -0400

    Setup header view.

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -39,7 +39,24 @@ let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
 struct ExerciseView: View {
     let index: Int
     var body: some View {
-        Text(exerciseNames[index])
+        VStack {
+            VStack {
+                Text(exerciseNames[index])
+                    .font(.largeTitle)
+                HStack {
+                    Image(systemName: "1.circle")
+                    Image(systemName: "2.circle")
+                    Image(systemName: "3.circle")
+                    Image(systemName: "4.circle")
+                }
+                .font(.title2)
+            }
+            Text("Video Player")
+            Text("Timer")
+            Text("Start/Done button")
+            Text("Rating")
+            Text("History button")
+        }
     }
 }
 
